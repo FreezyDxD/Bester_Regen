217e006078692c418fbbce0386b10f7c54e1294c
FreezyDxD <paulkotscha@gmail.com>
2026-05-29 22:04:12 +0200

    Use raw GitHub content URL for data files

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = '/Bester_Regen/data';
+const dir = 'https://raw.githubusercontent.com/freezydxd/Bester_Regen/main/data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
